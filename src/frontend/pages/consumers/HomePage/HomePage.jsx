4a3ed070233e67bff604c8702f86f1c06c08ef18
Konstantin Ligus <ligus.konstantin@gmail.com>
2023-09-10 20:51:41 +0300

    merged

--- src/frontend/pages/consumers/HomePage/HomePage.jsx
+++ src/frontend/pages/consumers/HomePage/HomePage.jsx
@@ -1,9 +1,9 @@
 import Banner from "@/frontend/components/consumers/Banner/Banner";
 
 export default async function HomePage() {
-    return (
-        <>
-            <Banner />
-        </>
-    );
+  return (
+    <>
+      <Banner />
+    </>
+  );
 }
