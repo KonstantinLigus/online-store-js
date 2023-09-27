7f56aaae862b5bb83521ba9397aad53015d7c9f4
Konstantin Ligus <ligus.konstantin@gmail.com>
2023-09-27 19:37:35 +0300

    fix response logic in tryCatchWrapper
    add "status" field

--- src/backend/items/helpers/getClientTryCatchWrapper.js
+++ src/backend/items/helpers/getClientTryCatchWrapper.js
@@ -4,8 +4,8 @@ import { getNotFoundItemError } from "./getErrorObj";
 export async function getClientTryCatchWrapper(callback) {
   return async function (args) {
     try {
-      const { status, ...response } = await callback(args);
-      return NextResponse.json(response, { status });
+      const res = await callback(args);
+      return NextResponse.json(res);
     } catch (error) {
       if (error.kind === "ObjectId") {
         return NextResponse.json(getNotFoundItemError(error.value));
