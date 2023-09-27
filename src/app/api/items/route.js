f7a4232981551245c32c7b63a0aad271baec6c6f
Konstantin Ligus <ligus.konstantin@gmail.com>
2023-09-27 19:12:26 +0300

    fix items route logic
    add additional condition for empty param category

--- src/app/api/items/route.js
+++ src/app/api/items/route.js
@@ -2,9 +2,14 @@ import getItemsController from "@/backend/items";
 
 export async function GET(req) {
   const { searchParams } = new URL(req.url);
-  const categories = searchParams.get("category");
+  const categories = searchParams.get("categories");
   const getfilteredItems = await getItemsController(
     "GET_FILTERED_ITEMS_ON_CLIENT",
   );
-  return await getfilteredItems({ categories });
+  if (categories) {
+    return await getfilteredItems({ categories });
+  }
+  if (!categories) {
+    return await getfilteredItems({});
+  }
 }
