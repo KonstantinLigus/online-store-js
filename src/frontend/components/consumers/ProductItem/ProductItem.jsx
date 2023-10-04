b7e09819a19d6056cb6e862b54e1f5dad2d6366f
Volodymyr Kulyk <kulyk.vr@gmail.com>
2023-10-04 13:04:59 +0300

    temp

--- src/frontend/components/consumers/ProductItem/ProductItem.jsx
+++ src/frontend/components/consumers/ProductItem/ProductItem.jsx
@@ -1,7 +1,7 @@
 import React from "react";
+import Link from "next/link";
 import styles from "./ProductItem.module.scss";
 import Image from "next/image";
-import Link from "next/link";
 
 const ProductItem = ({ id, title, description, price, mainImage }) => {
   return (
--- src/frontend/pages/consumers/ProductPage/ProductPage.jsx
+++ src/frontend/pages/consumers/ProductPage/ProductPage.jsx
@@ -1,5 +1,3 @@
-"use client";
-import React, { useEffect, useState } from "react";
 import styles from "./ProductPage.module.scss";
 import Image from "next/image";
 import product from "../../../../../public/assets/product.png";
@@ -26,69 +24,11 @@ const characteristic = [
 ];
 
 const ProductPage = props => {
-  const productId = props.product;
-
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    /*
-const getItemById = await getItemsController("GET_ONE_ITEM");
-  const { item } = await getItemById(id);
-*/
-    const fetchData = async () => {
-      const res = await fetch("api/items");
-      const { items } = await res.json();
-      let currentProduct = items.find(item => item._id == productId);
-      // тут отримуємо всі елементи, а після цього знаходимо потрібний за id
-      // думаю, що краще мати конкретний запит до бази даних за id
-
-      setData(currentProduct);
-    };
-    fetchData();
-  }, []);
+  console.log(props);
 
   return (
     <div className={styles.productCard}>
-      <h2 className={styles.productName}>{data.title}</h2>
-      <div className={styles.imageContainer}>
-        <Image
-          className={styles.imageProduct}
-          src={product}
-          /*src={data.mainImage}* - дивна поведінка: картинка відображається, а консоль пише, що зображення не має src*/
-          alt="product"
-          fill
-        />
-        <div className={styles.heartIconContainer}>
-          <Image
-            src={heart}
-            alt="heart-icon"
-            fill
-            className={styles.heartIcon}
-          />
-        </div>
-      </div>
-      <div className={styles.priceInformation}>
-        <p className={styles.price}>{data.price}</p>
-        <p className={styles.measure}>1 кг</p>
-      </div>
-      <button className={styles.button}>Додати в кошик</button>
-
-      <div className={styles.characteristic}>
-        <h2 className={styles.headline}>Характеристики</h2>
-
-        {characteristic.map(item => (
-          <div key={item.id}>
-            <div className={styles.heading}>
-              <Image src={information} alt="information" width={14} />
-              <h2 className={styles.title}>{item.title}</h2>
-            </div>
-            <p className={styles.description}>{item.description}</p>
-          </div>
-        ))}
-      </div>
-      <ProductList
-        className={styles.productList}
-        title="З цим товаром купують"
-      />
+      <h2 className={styles.productName}>f{props.id}</h2>
     </div>
   );
 };
