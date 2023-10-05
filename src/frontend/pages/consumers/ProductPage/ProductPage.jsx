6b773918cad4313470a177817971935dacd7f8af
Kulyk-Volodymyr <105850404+Kulyk-Volodymyr@users.noreply.github.com>
2023-10-05 10:37:02 +0300

    add conditional rendering

--- src/frontend/pages/consumers/ProductPage/ProductPage.jsx
+++ src/frontend/pages/consumers/ProductPage/ProductPage.jsx
@@ -37,49 +37,51 @@ const ProductPage = ({ params }) => {
   }, []);
 
   return (
-    <div className={styles.productCard}>
-      <h2 className={styles.productName}>{data.title}</h2>
-      <div className={styles.imageContainer}>
-        <Image
-          className={styles.imageProduct}
-          src={product}
-          //src={data.mainImage} - Browser console: Image is missing required "src"
-          alt="product"
-          fill
-        />
-        <div className={styles.heartIconContainer}>
+    data && (
+      <div className={styles.productCard}>
+        <h2 className={styles.productName}>{data.title}</h2>
+        <div className={styles.imageContainer}>
           <Image
-            src={heart}
-            alt="heart-icon"
+            className={styles.imageProduct}
+            src={product}
+            //src={data.mainImage} - Browser console: Image is missing required "src"
+            alt="product"
             fill
-            className={styles.heartIcon}
           />
-        </div>
-      </div>
-      <div className={styles.priceInformation}>
-        <p className={styles.price}>{data.price}</p>
-        <p className={styles.measure}>1 кг</p>
-      </div>
-      <button className={styles.button}>Додати в кошик</button>
-
-      <div className={styles.characteristic}>
-        <h2 className={styles.headline}>Характеристики</h2>
-
-        {characteristic.map(item => (
-          <div key={item.id}>
-            <div className={styles.heading}>
-              <Image src={information} alt="information" width={14} />
-              <h2 className={styles.title}>{item.title}</h2>
-            </div>
-            <p className={styles.description}>{item.description}</p>
+          <div className={styles.heartIconContainer}>
+            <Image
+              src={heart}
+              alt="heart-icon"
+              fill
+              className={styles.heartIcon}
+            />
           </div>
-        ))}
+        </div>
+        <div className={styles.priceInformation}>
+          <p className={styles.price}>{data.price}</p>
+          <p className={styles.measure}>1 кг</p>
+        </div>
+        <button className={styles.button}>Додати в кошик</button>
+  
+        <div className={styles.characteristic}>
+          <h2 className={styles.headline}>Характеристики</h2>
+  
+          {characteristic.map(item => (
+            <div key={item.id}>
+              <div className={styles.heading}>
+                <Image src={information} alt="information" width={14} />
+                <h2 className={styles.title}>{item.title}</h2>
+              </div>
+              <p className={styles.description}>{item.description}</p>
+            </div>
+          ))}
+        </div>
+        <ProductList
+          className={styles.productList}
+          title="З цим товаром купують"
+        />
       </div>
-      <ProductList
-        className={styles.productList}
-        title="З цим товаром купують"
-      />
-    </div>
+    )
   );
 };
 
