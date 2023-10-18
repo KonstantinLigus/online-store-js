d5f941324ea0f15ac153932c02e4cdd7fa06ab85
Konstantin Ligus <ligus.konstantin@gmail.com>
2023-10-18 17:31:08 +0300

    change price field type to [Number] within item.schema

--- src/backend/items/item.schema.js
+++ src/backend/items/item.schema.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const itemSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  price: { type: Number, required: true },
+  price: { type: [Number], required: true },
   images: { type: [String] },
   mainImage: { type: String },
   category: { type: [String] },
