c4beb28498fa32385bea5b108539344627f759da
Konstantin Ligus <ligus.konstantin@gmail.com>
2023-11-02 21:59:00 +0200

    change item schema

--- src/backend/entities/items/item.schema.js
+++ src/backend/entities/items/item.schema.js
@@ -3,14 +3,19 @@ import mongoose from "mongoose";
 export const itemSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  price: { type: [Number], required: true },
+  prices: [
+    {
+      price: { type: Number, required: true },
+      actionPrice: { type: Number },
+      value: { type: Number, required: true },
+      unit: { type: String, required: true },
+    },
+  ],
   images: { type: [String] },
   mainImage: { type: String },
   category: { type: [String], required: true },
   label: { type: [String] },
   producer: { type: String },
-  measurement: { unit: String, values: [Number] },
-  action: { type: Number },
 });
 
 export const Item = mongoose.models.item || mongoose.model("item", itemSchema);
