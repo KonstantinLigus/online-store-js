dd5435bf0b04a81ec28deeb2357b6b624b9bd07a
Mykokola <119622278+Mykokola@users.noreply.github.com>
2023-12-01 18:59:32 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-};
-
-module.exports = nextConfig;
+    images: {
+      domains: [process.env.IMAGES_DOMAIN],
+      hostname: "",
+    },
+  };
