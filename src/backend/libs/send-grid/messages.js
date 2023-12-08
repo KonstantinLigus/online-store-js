2a518552f37ceabe28bd7f92799405dd72fd2877
Konstantin Ligus <ligus.konstantin@gmail.com>
2023-12-08 09:15:40 +0200

    change messages.js

--- src/backend/libs/send-grid/messages.js
+++ src/backend/libs/send-grid/messages.js
@@ -3,6 +3,6 @@ export function createVerifyEmailMessage({ emailForVerify, linkForVerify }) {
     from: process.env.FROM_EMAIL_SEND,
     to: emailForVerify,
     subject: "Eco Store email verification",
-    html: `<head><style>*{font-size: 14px;}</style></head><h3>You are currently sign Up to our site<h3><p>Please, follow this <a href=${linkForVerify}>link</a> to verify your email</p>`,
+    html: `<head><style>*{color: green;}</style></head><h3>You are currently sign Up to our site</h3><p>Please, follow this <a href=${linkForVerify}>link</a> to verify your email</p>`,
   };
 }
