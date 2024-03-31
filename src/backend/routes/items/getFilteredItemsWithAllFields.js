d735f71cc6cf13c42ba74fe0423c127d4d627bdf
Konstantin Ligus <ligus.konstantin@gmail.com>
2024-03-31 16:58:58 +0300

    add getAllItemsWithAllFields

---
+++ src/backend/routes/items/getFilteredItemsWithAllFields.js
@@ -0,0 +1,17 @@
+import itemsControllers from "@/backend/entities/items";
+import { getTryCatchWrapper } from "@/backend/helpers/tryCatchWrapper";
+
+export async function getFilteredItems(req) {
+  const { searchParams } = new URL(req.url);
+
+  let objQuery = {};
+  if (searchParams.size !== 0)
+    searchParams.forEach((value, key) =>
+      Object.assign(objQuery, { [key]: new RegExp(value, "i") }),
+    );
+
+  const data = await itemsControllers.getAllItemsWithAllFieldsByField(objQuery);
+  return data;
+}
+
+export default getTryCatchWrapper(getFilteredItems);
