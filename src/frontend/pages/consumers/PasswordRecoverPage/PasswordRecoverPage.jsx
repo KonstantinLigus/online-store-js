1b3894678f83a53ea5e6239a5768805dfdcf9e51
Konstantin Ligus <ligus.konstantin@gmail.com>
2024-06-16 14:52:27 +0300

    add password recovery page

---
+++ src/frontend/pages/consumers/PasswordRecoverPage/PasswordRecoverPage.jsx
@@ -0,0 +1,7 @@
+import PasswordRecoveryForm from "@/frontend/components/consumers/PasswordRecoveryForm";
+
+const PasswordRecoverPage = () => {
+  return <PasswordRecoveryForm />;
+};
+
+export default PasswordRecoverPage;
