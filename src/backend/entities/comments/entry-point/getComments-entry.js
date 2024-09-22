97bcbc5970750b817a2771fad18eab25921defc5
Konstantin Ligus <ligus.konstantin@gmail.com>
2024-09-22 13:00:34 +0300

    add pagination to comment collection

--- src/backend/entities/comments/entry-point/getComments-entry.js
+++ src/backend/entities/comments/entry-point/getComments-entry.js
@@ -1,7 +1,10 @@
 import { NextResponse } from "next/server";
 import { commentServices } from "../data-access/comment.services";
 
-export async function getCommentsEntry() {
-  const comments = await commentServices.getComments();
+export async function getCommentsEntry(req) {
+  const page = Number(req.nextUrl.searchParams.get("page")) || 1;
+  const limit = Number(req.nextUrl.searchParams.get("limit")) || 10;
+
+  const comments = await commentServices.getComments({ skip: page, limit });
   return NextResponse.json(comments, { status: 200 });
 }
