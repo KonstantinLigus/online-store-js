36bea45808dd8980ed1165646a7bbd6b0bdfb8bf
Konstantin Ligus <ligus.konstantin@gmail.com>
2024-11-24 12:20:42 +0200

    change items backend route
    if pagination absent return all items

--- src/backend/routes/items/getFilteredItemsWithAllFields.js
+++ src/backend/routes/items/getFilteredItemsWithAllFields.js
@@ -3,9 +3,9 @@ import { getTryCatchWrapper } from "@/backend/helpers/tryCatchWrapper";
 
 export async function getFilteredItems(req) {
   const { searchParams } = new URL(req.url);
-  const page = parseInt(searchParams.get("page")) || 1;
-  const limit = parseInt(searchParams.get("limit")) || 10;
-  const skip = (parseInt(page) - 1) * limit;
+  const page = parseInt(searchParams.get("page"));
+  const limit = parseInt(searchParams.get("limit"));
+  const skip = (page - 1) * limit;
 
   let objQuery = {};
   if (searchParams.size !== 0)
